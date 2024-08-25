4822d10951962b9d24af9ddbb542f21252c1f64d
Dionysen <solongnight@outlook.com>
2024-08-25 17:47:28 +0800

    move gobang-server to windows

--- gobang-client/online/tcpclient.h
+++ gobang-client/online/tcpclient.h
@@ -20,12 +20,13 @@
 #include <unistd.h>
 #endif
 
-#define SERVER_IP "82.157.246.225"
-// #define SERVER_IP "127.0.0.1"
+#define SERVER_IP "10.8.154.184"
+
 #define SERVER_PORT 16556
 #define BUFFSIZE 1024
 
-class tcpclient {
+class tcpclient
+{
   private:
     struct sockaddr_in servaddr;
 #if WIN32
@@ -38,35 +39,38 @@ class tcpclient {
 
   public:
     tcpclient();
-    int getSockFD() { return sockfd; }
-    bool connectToServer(); // connect to server
-    void closesocket();     // close socket
-    void disconnect();      // disconnect to server
+    int getSockFD()
+    {
+        return sockfd;
+    }
+    bool connectToServer();  // connect to server
+    void closesocket();      // close socket
+    void disconnect();       // disconnect to server
 
   public:
-    void static parseInformation(onlinegame &Game, lobby &Lobby,
-                                 std::string buff); // Receive and parse info
+    void static parseInformation(onlinegame& Game, lobby& Lobby,
+                                 std::string buff);  // Receive and parse info
 
     // slots：
-    bool joinLobby();            // join lobby
-    void createRoom();           // create a room
-    void joinRoom(int roomID);   // join a room
-    void watchMatch(int roomID); // watch a game
+    bool joinLobby();             // join lobby
+    void createRoom();            // create a room
+    void joinRoom(int roomID);    // join a room
+    void watchMatch(int roomID);  // watch a game
 
-    void quitLobby(); // quit lobby and disconnect to server
-    void quitRoom();  // quit room and go to lobby
+    void quitLobby();  // quit lobby and disconnect to server
+    void quitRoom();   // quit room and go to lobby
 
-    void prepared();                 // prepare
-    void drop(int x, int y);         // drop
-    void requestRetract();           // retract
-    void repondRetract(bool anwser); // respond retracting
-    void requestReplay();            // replay
-    void repondReplay(bool anwser);  // respond replay
-    void exit();                     // exit
-    void concede();                  // concede
-    void restartGame();              // restart a game
+    void prepared();                  // prepare
+    void drop(int x, int y);          // drop
+    void requestRetract();            // retract
+    void repondRetract(bool anwser);  // respond retracting
+    void requestReplay();             // replay
+    void repondReplay(bool anwser);   // respond replay
+    void exit();                      // exit
+    void concede();                   // concede
+    void restartGame();               // restart a game
 
     void setPlayerName(QString name);
 };
 
-#endif // TCPCLIENT_H
+#endif  // TCPCLIENT_H
